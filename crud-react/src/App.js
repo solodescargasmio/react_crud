f9b81f43626e53454919664e61f9c28ee578c2be
solodescargasmio <solodescargasmio@gmail.com>
2022-02-22 18:47:09 -0300

    Visualizar tabla y primer uso de Hook

--- crud-react/src/App.js
+++ crud-react/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import UserTable from "./components/UserTable";
+import {v4 as uuidv4} from 'uuid';
+
+
 
 function App() {
+  const usersData = [
+    { id: uuidv4(), name:'Tania', username:'flopialgo' },
+    { id: uuidv4(), name:'Pablo', username:'Por que no' },
+    { id: uuidv4(), name:'Manuel', username:'Huyele' },
+  ]
+
+  const [users, setUsers] = useState(usersData)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+       <h1>CRUD App con Hooks</h1> 
+       <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTable users = {usersData}/>
+        </div>
+       </div>
     </div>
   );
 }
